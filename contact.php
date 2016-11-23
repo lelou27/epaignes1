b53e63efc650f4b53708c1eeb09d2c83dec2fb92
leandre <lelou.pro@gmail.com>
2016-11-23 11:36:18 +0100

    modif branche 1

--- contact.php
+++ contact.php
@@ -11,6 +11,15 @@ echo "c'est ma page de contact";
     <title></title>
   </head>
   <body>
+    <form class="" action="index.html" method="post">
+      <label for="name"></label>
+      <input type="text" name="" value="">
+
+      <label for="porenopm"></label>
+      <input type="text" name="" value="">
+
+      <input type="submit" name="" value="">
+    </form>
 
     <h1>C'est ma page contact !!!!!!!!!!!!!</h1>
 
