cd2cb4d5582400c3b7d7129fd51fee168234445d
leandre <lelou.pro@gmail.com>
2016-11-23 10:55:17 +0100

    lol

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+echo "ldkfslkdfndslknfs";
+echo "c'est ma page de contact";
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <h1>C'est ma page contact !!!!!!!!!!!!!</h1>
+
+  </body>
+</html>
